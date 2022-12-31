39e45b9a13dacb46dadb581ba933a7208f516f15
Manir Uddin <manir22sust@gmail.com>
2022-12-31 12:44:10 +0100

    MERN Stack frontend completed

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+
+export const Navbar = () => {
+  return (
+    <header>
+      <div className="container">
+        <Link to="/">
+          <h1> Workout Buddy</h1>
+        </Link>
+      </div>
+    </header>
+  );
+};
